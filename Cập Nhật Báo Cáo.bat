f9fd74e2254aee3db347767055d2632619ae0cbe
T&T Vina <report@ttvina.com>
2026-05-29 15:20:34 +0700

    Updates

--- Cập Nhật Báo Cáo.bat
+++ Cập Nhật Báo Cáo.bat
@@ -1,17 +1,22 @@
 @echo off
 chcp 65001 > nul
 echo ----------------------------------------------------
-echo ĐANG CẬP NHẬT BÁO CÁO GIỮ CHÂN VIDEO (FACEBOOK)...
+echo ĐANG CẬP NHẬT BÁO CÁO GIỮ CHÂN VIDEO - FACEBOOK
 echo ----------------------------------------------------
 echo.
 "C:\Users\OS\AppData\Local\Python\bin\python.exe" "d:\T&TVina\Report\create_report.py"
 echo.
 echo ----------------------------------------------------
-echo ĐANG TỰ ĐỘNG ĐẨY BÁO CÁO LÊN GITHUB PAGES...
+echo ĐANG TỰ ĐỘNG ĐẨY BÁO CÁO LÊN GITHUB - NHÁNH MAIN
 echo ----------------------------------------------------
 git add .
 git commit -m "Auto-update report: %date% %time%"
 git push origin main
+echo.
+echo ----------------------------------------------------
+echo ĐANG XUẤT BẢN LÊN WEBSITE - GITHUB PAGES
+echo ----------------------------------------------------
+node "d:\T&TVina\Report\node_modules\gh-pages\bin\gh-pages.js" -d "d:\T&TVina\Report"
 echo.
 echo ----------------------------------------------------
 echo Đã cập nhật thành công và đồng bộ lên link web!
